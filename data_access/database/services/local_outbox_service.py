48057c91af4600e904e3072aa7508780e397114b
Marius <marius.vrstr@gmail.com>
2024-06-02 16:02:06 +0200

    Pooling updates and thresholds

--- data_access/database/services/local_outbox_service.py
+++ data_access/database/services/local_outbox_service.py
@@ -11,6 +11,7 @@ from orchestrator_sdk.data_access.database.entities.message_outbox_entity import
 from orchestrator_sdk.contracts.publishing.publish_envelope import PublishEnvelope
 from orchestrator_sdk.data_access.message_broker.methods.api_submission import ApiSubmission
 from orchestrator_sdk.data_access.database.outbox_status import OutboxStatus
+from orchestrator_sdk.data_access.database.tools.pooling_utility import PoolingUtility
 from requests.exceptions import ConnectionError as RequestsConnectionError
 
 from orchestrator_sdk.seedworks.logger import Logger
@@ -25,18 +26,19 @@ class LocalOutboxService:
     remaining_count:Optional[int] = None    
     min_cleanup_interval_in_hours:int = 1
     pending_message_counts:dict = None    
+    pooling_utility:PoolingUtility = None
     
     BATCH_SIZE:int = 80
     BATCH_WAIT_TIME_IN_SECONDS:int = 60
     SLACK_MARKER:int = 20
     SLACK_WAIT_TIME_IN_SECONDS:int = 1
     CONCURENT_LIMIT:int = 10
-    DB_SLEEP_IN_SECONDS:int = 5
-    DB_POOL_OVERFLOW_THRESHOLD:int = 20
+    MESSAGE_DELAY_ON_FAILURE_IN_MINUTES:int = 5
     
     def __init__(self, message_database:MessageDatabase): 
         self.is_busy = False
         self.message_database = message_database
+        self.pooling_utility = PoolingUtility(logger, self.message_database.db_engine)
         
     def update_message_counter_on_success(self, message_name):
         
@@ -87,7 +89,7 @@ class LocalOutboxService:
         async with LocalOutboxService.lock: 
             LocalOutboxService.is_busy = False
             
-    async def process_item(self, session:Session, submitter:ApiSubmission, message:MessageOutboxEntity, content:str) -> bool: 
+    async def process_item(self, submitter:ApiSubmission, message:MessageOutboxEntity, content:str) -> bool: 
         try:
             
             items_at_source:Optional[int] = None
@@ -112,64 +114,38 @@ class LocalOutboxService:
             message.status = OutboxStatus.Published.name
             message.is_completed = True
             message.published_date = datetime.utcnow()
-            session.commit()
-            
+                        
             self.update_message_counter_on_success(message_name=message.message_name)           
 
             return True
         
         except RequestsConnectionError:
             logger.warn(f'Unable to connect to orchestrator server to publish the messages waiting to be sent. Will delay retry.')
-            logger.info(f'MESSAGE DB POOL STATUS: [{self.message_database.db_engine.pool.status()}]')
-            session.rollback()
+            logger.info(f'MESSAGE DB POOL STATUS: [{self.message_database.db_engine.pool.status()}]')           
+
             return False
             
         except Exception as ex:
             logger.error(f"Oops! {ex.__class__} occurred. Details: {ex}")
             logger.info(f'MESSAGE DB POOL STATUS: [{self.message_database.db_engine.pool.status()}]')
-            session.rollback()
-            raise   
+            
+            delay_period = timedelta(minutes=self.MESSAGE_DELAY_ON_FAILURE_IN_MINUTES)
+            eligible_after = datetime.utcnow() + delay_period            
+            message.eligible_after = eligible_after            
+        
+            return False
    
-    async def process_all(self, messages, session:Session, api_submission:ApiSubmission) -> int:
+    async def process_concurrent_batch(self, messages, api_submission:ApiSubmission) -> int:
 
         # Create a list of coroutine objects by calling the process function on each item
-        tasks = [self.process_item(session, api_submission, message, message.publish_request_object) for message in messages]
+        tasks = [self.process_item(api_submission, message, message.publish_request_object) for message in messages]
 
         # Wait for all tasks to complete and gather their results
-        results = await asyncio.gather(*tasks)        
-        successes = sum(results)
+        results = await asyncio.gather(*tasks)
         
-        return successes
-    
-    async def throttle_database_connections(self):
-        
-        max_delays = 15
-        check_pooling = True
-        delay_counter = 0
-        db_pool = self.message_database.db_engine.pool
-        
-        if (db_pool == None):
-            return
-        
-        while (check_pooling and (delay_counter < max_delays)):               
-            
-            pool_limit = db_pool.size()
-            pool_overflow = self.DB_POOL_OVERFLOW_THRESHOLD
-            pool_in_use =  db_pool.checkedout()
-            
-            danger_zone = (pool_overflow-pool_limit) / 2
-            
-            if (pool_in_use > pool_limit):
-                logger.warn(f"There are more connections open [{pool_in_use}] than the limit [{pool_limit}] it will start to fail when it reaches [{pool_overflow}]")
-            
-            if pool_in_use < (pool_limit + danger_zone):
-                check_pooling = False
-                
-            else:
-                delay_counter += 1
-                logger.warn(f"Overflow is reaching dangerous levels. Delaying batch submission [{delay_counter}/{max_delays}]. The Limit: [{pool_limit}], Total Connections: [{pool_in_use}] and Overflow Threshold: [{pool_overflow}]")
-                await asyncio.sleep(self.DB_SLEEP_IN_SECONDS)
-                           
+        successes = sum(results)        
+        return successes   
+
    
     async def process_next_batch(self):
         
@@ -204,13 +180,12 @@ class LocalOutboxService:
                 self.pending_message_counts = None
                 logger.warn(f'Timeout while cleaning up outbox (Cleanup | Remove Duplicates)] - Not stopping, continuing with outbox processing.')                
                 
-            await self.throttle_database_connections()
+            await self.pooling_utility.throttle_database_connections()
                 
             batch_result:ReadyForSubmissionBatch = await asyncio.wait_for(outbox_repo.get_next_messages(batch_size=self.BATCH_SIZE), timeout=60) 
             
             if (batch_result == None or batch_result.messages_not_completed <= 0):
                 another_batch = False
-                session.rollback()
                 
             else:
                 logger.info(f'OUTBOX Queue Summary >>>> Remaining [{len(batch_result.messages)}/{batch_result.messages_not_completed}] Ready [{batch_result.messages_ready}] Intervention [{batch_result.messages_needing_intervention}] <<<<')
@@ -222,9 +197,14 @@ class LocalOutboxService:
                 batch_size_at_start = len(batch_result.messages)                                
                 
                 while (len(batch_result.messages) > 0):
+                    
+                    await self.pooling_utility.throttle_database_connections()
+                    
                     batch_size = min(self.CONCURENT_LIMIT, len(batch_result.messages))
                     concurrent_subset = [batch_result.messages.pop() for _ in range(batch_size)]
-                    successes = await self.process_all(concurrent_subset, session, api_submission)
+                    successes = await self.process_concurrent_batch(concurrent_subset, api_submission)
+                    
+                    session.commit()
                     
                     success_count += successes        
                     slack_count += successes
@@ -245,13 +225,10 @@ class LocalOutboxService:
                 no_remaining_items_are_ready = True if remaining != None and ready != None and ready == 0 else False
                 
                 delay_next_request = something_went_wrong_now or something_went_wrong_previously or no_remaining_items_are_ready
-
-                session.commit()
        
         except Exception as ex:     
                 logger.error(f"Oops! {ex.__class__} process_next_batch: {ex}")
                 logger.info(f'MESSAGE DB POOL STATUS: [{self.message_database.db_engine.pool.status()}]')
-                session.rollback()
                 delay_next_request = True                
                 
         finally:
@@ -270,6 +247,8 @@ class LocalOutboxService:
             
     async def cleanup(self, outbox_repo: MessageOutboxRepository):  
         try:
+            
+            await self.pooling_utility.throttle_database_connections()
             
             isolated_session = self.message_database.db_session_maker()            
             await outbox_repo.delete_old_message_history(isolated_session)
