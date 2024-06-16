d957b7dc98f906ac5a50c608db21d1ee013e631c
Marius Vorster <marius.vrstr@gmail.com>
2024-06-16 21:22:41 +0200

    Moved to milisecond configuration for more control

--- testing/workload_simulator.py
+++ testing/workload_simulator.py
@@ -7,24 +7,24 @@ import asyncio
 import random
 
 class WorkloadSimulator(BaseModel):
-    blocked_percentage_in_seconds: Optional[int] = None
+    blocked_percentage_in_milliseconds: Optional[int] = None
     exception_percentage: Optional[int] = None
     permanent_exception_percentage: Optional[int] = None
     fastest_time: int = 0
     slowest_time: int = 0
         
-    def set_execution_span(self, fastest_time_in_seconds: int, slowest_time_in_seconds: int) -> 'WorkloadSimulator':
-        if (fastest_time_in_seconds > slowest_time_in_seconds):
-            raise Exception(f'Please make sure your fastest time [{fastest_time_in_seconds}] is smaller than your slowest time [{slowest_time_in_seconds}]')  
+    def set_execution_span(self, fastest_time_in_milliseconds: int, slowest_time_in_milliseconds: int) -> 'WorkloadSimulator':
+        if (fastest_time_in_milliseconds > slowest_time_in_milliseconds):
+            raise Exception(f'Please make sure your fastest time [{fastest_time_in_milliseconds}] is smaller than your slowest time [{slowest_time_in_milliseconds}]')  
         
-        self.fastest_time = fastest_time_in_seconds
-        self.slowest_time = slowest_time_in_seconds
+        self.fastest_time = fastest_time_in_milliseconds
+        self.slowest_time = slowest_time_in_milliseconds
         
         return self 
         
        
-    def set_blocked_percentage_of_execution_time(self, blocked_percentage_in_seconds:int) -> 'WorkloadSimulator':
-        self.blocked_percentage_in_seconds = blocked_percentage_in_seconds
+    def set_blocked_percentage_of_execution_time(self, blocked_percentage_in_milliseconds:int) -> 'WorkloadSimulator':
+        self.blocked_percentage_in_milliseconds = blocked_percentage_in_milliseconds
         return self
     
     def set_exception_thresholds(self, exception_percentage:int, permanent_exception_percentage:Optional[int] = None) -> 'WorkloadSimulator':
@@ -32,7 +32,7 @@ class WorkloadSimulator(BaseModel):
         self.permanent_exception_percentage = permanent_exception_percentage
         return self    
 
-    async def run(self, percentage_faster:Optional[int] = None, percentage_slower:[int] = None) -> int:
+    async def run(self, percentage_faster:Optional[int] = None, percentage_slower:int = None) -> int:
         start_time = time.time()
         
         distance = self.slowest_time- self.fastest_time
@@ -48,14 +48,14 @@ class WorkloadSimulator(BaseModel):
             adjustment = round(distance * (percentage_slower/100))
             fastest = fastest + adjustment               
         
-        total_duration_in_seconds:int = random.randint(fastest, slowest)
-        blocked_duration_in_seconds:Optional[float] = total_duration_in_seconds * (self.blocked_percentage_in_seconds/100) if self.blocked_percentage_in_seconds != None and self.blocked_percentage_in_seconds > 0 else None
+        total_duration_in_milliseconds:int = random.randint(fastest, slowest)
+        blocked_duration_in_milliseconds:Optional[float] = total_duration_in_milliseconds * (self.blocked_percentage_in_milliseconds/100) if self.blocked_percentage_in_milliseconds != None and self.blocked_percentage_in_milliseconds > 0 else None
         
-        if blocked_duration_in_seconds != None and blocked_duration_in_seconds > 0:
-            time.sleep(blocked_duration_in_seconds)
+        if blocked_duration_in_milliseconds != None and blocked_duration_in_milliseconds > 0:
+            time.sleep(blocked_duration_in_milliseconds/1000)
             
-        non_blocking_duration:float = total_duration_in_seconds if blocked_duration_in_seconds == None else total_duration_in_seconds - blocked_duration_in_seconds        
-        await asyncio.sleep(non_blocking_duration) 
+        non_blocking_duration:float = total_duration_in_milliseconds if blocked_duration_in_milliseconds == None else total_duration_in_milliseconds - blocked_duration_in_milliseconds        
+        await asyncio.sleep(non_blocking_duration/1000) 
         
         error_chance = random.randint(1, 100)
         
