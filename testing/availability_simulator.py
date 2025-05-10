9fae3ef9f6e576825bc6ede8e8c520a2a178538e
Marius Vorster <marius.vrstr@gmail.com>
2025-05-10 19:51:22 +0200

    Updated outage mechanism

--- testing/availability_simulator.py
+++ testing/availability_simulator.py
@@ -10,8 +10,9 @@ class ServiceUnavailableError(TimeoutError):
 class AvailabilitySimulator:
     """Cycle between ‘online’ and ‘offline’ in one shared singleton."""
 
-    offline_after: int = 30
-    max_offline_minutes: int = 5
+    failed_count = 0
+    offline_after: int = 60
+    max_offline_minutes: int = random.randint(2, 5)
     offline_duration:int[Optional] = None
 
     _instance: Optional["AvailabilitySimulator"] = None
@@ -35,21 +36,26 @@ class AvailabilitySimulator:
         now = datetime.utcnow()
 
         if self._available_after and now < self._available_after:
+            failed_count += 1
             return False                    # still offline
 
         if self._available_after:           # outage ended
             self._available_after = None
             self._last_cycle_start = now
+            self.failed_count = 0
             return True
 
         if now - self._last_cycle_start > timedelta(minutes=self.offline_after):
             self.offline_duration = random.randint(1, self.max_offline_minutes)
             self._available_after = now + timedelta(minutes=self.offline_duration)
+            failed_count += 1
             return False                    # just went offline
 
         return True                         # still online
 
     def simulate_unavailable(self) -> None:
+        remaining_time = datetime.utcnow() - self._available_after
+
         raise ServiceUnavailableError(
-            f"Simulated client offline for [{self.offline_duration}] minutes until [{self._available_after.isoformat(timespec='seconds')} UTC]"
+            f"Simulated client offline, remaining outage time [{remaining_time.seconds}s] - Blocked Requests: [{self.failed_count}], Duration: [{self.offline_duration}min], Outage End: [{self._available_after.isoformat(timespec='seconds')}]."
         )
