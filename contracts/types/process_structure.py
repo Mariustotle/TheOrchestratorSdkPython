022c175026bc7e7baad4c72acdf2ca213fa86be9
Marius Vorster <marius.vrstr@gmail.com>
2025-06-01 13:36:42 +0200

    Updated Processing Structures

--- contracts/types/process_structure.py
+++ contracts/types/process_structure.py
@@ -5,7 +5,8 @@ class ProcessStructure(Enum):
     Undefined = 0,
     Commands = 1,
     Events = 2,
-    EventStream = 3,
-    EventSnapshot = 4,
-    Batch = 5,
-    Workflow = 6
+    StreamInbound = 3,
+    StreamOutbound = 4,
+    Snapshot = 5,
+    Batch = 6,
+    Workflow = 7
