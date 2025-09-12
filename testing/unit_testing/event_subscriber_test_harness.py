396584bf566dcd32b62a20ac12725dc9114cc02a
Marius Vorster <mariusv@tradeshield.ai>
2025-09-12 09:56:45 +0200

    Merge update from TS branch

--- testing/unit_testing/event_subscriber_test_harness.py
+++ testing/unit_testing/event_subscriber_test_harness.py
@@ -1,5 +1,8 @@
 from orchestrator_sdk.message_processors.events.event_subscriber_base import EventSubscriberBase
 from orchestrator_sdk.testing.unit_testing.process_context_builder import ProcessContextBuilder
+from orchestrator_sdk.seedworks.logger import Logger
+
+logger = Logger.get_instance()
 
 class EventSubscriberTestHarness:
 
@@ -12,14 +15,20 @@ class EventSubscriberTestHarness:
 
     async def process(self, callback_message):
 
-        if (type(callback_message) != self.subscriber.request_type):
-            raise Exception(f'Canot intiate subscriber [{type(self.subscriber)}] with [{type(callback_message)}] expecting [{self.subscriber.request_type}]')
+        try:
+            if (type(callback_message) != self.subscriber.request_type):
+                raise Exception(f'Canot intiate subscriber [{type(self.subscriber)}] with [{type(callback_message)}] expecting [{self.subscriber.request_type}]')
 
-        process_context = self.process_context_builder.build()
-        return await self.subscriber._process(
-            request=callback_message, 
-            context=process_context, 
-            unit_of_work=None)
+            process_context = self.process_context_builder.build()
+            return await self.subscriber._process(
+                request=callback_message, 
+                context=process_context, 
+                unit_of_work=None)
+        
+        except Exception as ex:
+            logger.error(f"Oops! {ex.__class__} occurred. Details: {ex}")
+
+
 
     
 
